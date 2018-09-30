a2f103cf69a2e66e2eb54bef5df83785c5a4fdf0
Angelarmandol <Angelarmandol@hotmail.com>
2018-09-30 05:12:41 -0700

    ad instrumented class

--- AndroidStudioProject/app/src/androidTest/java/com/example/angel/calculadorabasica/ExampleInstrumentedTest.java
+++ AndroidStudioProject/app/src/androidTest/java/com/example/angel/calculadorabasica/ExampleInstrumentedTest.java
@@ -1,26 +1,48 @@
 package com.example.angel.calculadorabasica;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import com.example.angel.calculadorabasica.MainActivity;
+import android.support.test.filters.LargeTest;
 import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
 import org.junit.runner.RunWith;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import org.junit.Rule;
+import org.junit.Test;
+import android.support.test.rule.ActivityTestRule;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+@LargeTest
+public class ExampleIntrumentedTest{
+    @Rule
+    public ActivityTestRule<MainActivity> mActivity = new ActivityTestRule<>(MainActivity.class);
 
-        assertEquals("com.example.angel.calculadorabasica", appContext.getPackageName());
+    @Test
+    public void testSuma(){
+
+        onView(withId(R.id.numero1))
+                .perform(ViewActions.click())
+                .perform(ViewActions.typeText("5"))
+                .check(ViewAssertions.matches(withText("5")))
+                .perform(ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.numero2)
+                .perform(ViewActions.click())
+                .perform(ViewActions.typeText("3"))
+                .check(ViewAssertions.matches(withText("3")))
+                .perform(ViewActions.closeSoftKeyboard());
+
+        onView(withId(R.id.buttonMulti)
+                .perform(ViewActions.click());
+
+        onView(withId(R.id.resultado))
+                .check(ViewAssertions.matches(withText("15")));
+
+
     }
+
+
 }
--- AndroidStudioProject/app/src/main/java/com/example/angel/calculadorabasica/MainActivity.java
+++ AndroidStudioProject/app/src/main/java/com/example/angel/calculadorabasica/MainActivity.java
@@ -2,12 +2,71 @@ package com.example.angel.calculadorabasica;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int a = 0;
+    int b = 0;
+
+
+    TextView resultado;
+    EditText numero1, numero2;
+    Button buttonMulti, buttonDivi, buttonResta, buttomSuma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        resultado =(TextView) findViewById(R.id.resultado);
+        numero1 = (EditText) findViewById(R.id.numero1);
+        numero2 = (EditText) findViewById(R.id.numero2);
+
+        buttonMulti = (Button) findViewById(R.id.buttonMulti);
+        buttonDivi = (Button) findViewById(R.id.buttonDivi);
+        buttonResta = (Button) findViewById(R.id.buttonResta);
+        buttomSuma = (Button) findViewById(R.id.buttomSuma);
+
+
+        buttonMulti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               a =  Integer.parseInt(numero1.getText().toString());
+               b =  Integer.parseInt(numero2.getText().toString());
+                multiplicacion(a,b);
+
+            }
+        });
+
+
+
+
+
+    }// end onCreate
+
+
+    public void multiplicacion(int datoA,int datoB){
+        int result =0;
+        try {
+            if ((datoA > 0) && (datoB > 0)) {
+                result = datoB + datoB;
+                resultado.setText(result);
+            }
+
+
+        } catch (NumberFormatException e) {
+            resultado.setText("Error");
+            Log.d("APP_CALCULATOR", "Error al realizar suma");
+        }
+
+
+
     }
 }
